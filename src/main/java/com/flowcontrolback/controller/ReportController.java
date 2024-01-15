634277a924886e4a73112724d7c9a165d43fe4ac
FelipeStefan0 <felipeopdeop@gmail.com>
2024-01-15 20:11:21 -0300

    wip: criando controller e service do relatório

---
+++ src/main/java/com/flowcontrolback/controller/ReportController.java
@@ -0,0 +1,11 @@
+package com.flowcontrolback.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/report")
+@RequiredArgsConstructor
+public class ReportController {
+}
---
+++ src/main/java/com/flowcontrolback/services/ReportService.java
@@ -0,0 +1,9 @@
+package com.flowcontrolback.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReportService {
+}
