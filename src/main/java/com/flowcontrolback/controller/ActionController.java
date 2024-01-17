955bba3d561faef7fdf2f9873e3149772b20e264
Felipe Stefano <felipe.stefano@parnamirim.rn.gov.br>
2024-01-17 15:49:09 -0300

    support: configuração do CORS local de cada request mapping

--- src/main/java/com/flowcontrolback/controller/ActionController.java
+++ src/main/java/com/flowcontrolback/controller/ActionController.java
@@ -14,40 +14,40 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/action")
 @RestController
+@CrossOrigin(origins = "http://localhost:8100")
 public class ActionController {
 
-    private final ActionService service;
+  private final ActionService service;
 
-    @GetMapping
-    public ResponseEntity<ApiResponse<List<Action>>> getAll() {
-        ApiResponse<List<Action>> response = new ApiResponse<>();
-        List<Action> listedAction = service.list();
-        response.of(HttpStatus.OK, "Listado com sucesso!", listedAction);
-        return ResponseEntity.status(response.getStatus()).body(response);
-    }
+  @GetMapping
+  public ResponseEntity<ApiResponse<List<Action>>> getAll() {
+    ApiResponse<List<Action>> response = new ApiResponse<>();
+    List<Action> listedAction = service.list();
+    response.of(HttpStatus.OK, "Listado com sucesso!", listedAction);
+    return ResponseEntity.status(response.getStatus()).body(response);
+  }
 
-    @PostMapping
-    public ResponseEntity<ApiResponse<Action>> create(@RequestBody Action action) {
-        ApiResponse<Action> response = new ApiResponse<>();
-        Action createdAction = service.create(action);
-        response.of(HttpStatus.OK, "Registrado com sucesso!", createdAction);
-        return ResponseEntity.status(response.getStatus()).body(response);
-    }
+  @PostMapping
+  public ResponseEntity<ApiResponse<Action>> create(@RequestBody Action action) {
+    ApiResponse<Action> response = new ApiResponse<>();
+    Action createdAction = service.create(action);
+    response.of(HttpStatus.OK, "Registrado com sucesso!", createdAction);
+    return ResponseEntity.status(response.getStatus()).body(response);
+  }
 
-    @DeleteMapping
-    public ResponseEntity<ApiResponse<Action>> delete(@RequestParam Long id) throws Exception {
-        ApiResponse<Action> response = new ApiResponse<>();
-        service.delete(id);
-        response.of(HttpStatus.OK, "Deletado com sucesso!");
-        return ResponseEntity.status(response.getStatus()).body(response);
-    }
+  @DeleteMapping
+  public ResponseEntity<ApiResponse<Action>> delete(@RequestParam Long id) throws Exception {
+    ApiResponse<Action> response = new ApiResponse<>();
+    service.delete(id);
+    response.of(HttpStatus.OK, "Deletado com sucesso!");
+    return ResponseEntity.status(response.getStatus()).body(response);
+  }
 
-    @PutMapping
-    public ResponseEntity<ApiResponse<Action>> edit(@RequestBody Action action) {
-        ApiResponse<Action> response = new ApiResponse<>();
-        Action actionChanged = service.edit(action);
-        response.of(HttpStatus.OK,"Resgistro modificado com sucesso!", actionChanged);
-        return ResponseEntity.status(response.getStatus()).body(response);
-    }
-
+  @PutMapping
+  public ResponseEntity<ApiResponse<Action>> edit(@RequestBody Action action) {
+    ApiResponse<Action> response = new ApiResponse<>();
+    Action actionChanged = service.edit(action);
+    response.of(HttpStatus.OK, "Resgistro modificado com sucesso!", actionChanged);
+    return ResponseEntity.status(response.getStatus()).body(response);
+  }
 }
