030500519219ba3522ed3a3cfeac16ab0b923e33
FelipeStefan0 <felipeopdeop@gmail.com>
2024-02-19 22:22:47 -0300

    feat: create report entity and table

---
+++ src/main/java/com/flowcontrolback/entities/Report.java
@@ -0,0 +1,34 @@
+package com.flowcontrolback.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Month;
+import java.time.Year;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "report", schema = "report")
+public class Report {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(name = "month", nullable = false)
+    private Month month;
+
+    @Column(name = "year", nullable = false)
+    private Integer year;
+
+    @Column(name = "total_value", nullable = false)
+    private Double totalValue;
+
+    @Column(name = "in_total_value", nullable = false)
+    private Double inTotalValue;
+
+    @Column(name = "out_total_value", nullable = false)
+    private Double outTotalValue;
+}
