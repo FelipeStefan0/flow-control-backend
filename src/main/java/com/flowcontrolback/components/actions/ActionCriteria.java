ac6e92a5edcf7b02a70c620fabcb2051165a7327
FelipeStefan0 <felipeopdeop@gmail.com>
2024-05-24 08:42:47 -0300

    support: adding jpaSpecification to Report Repository

---
+++ src/main/java/com/flowcontrolback/components/actions/ActionCriteria.java
@@ -0,0 +1,4 @@
+package com.flowcontrolback.components.actions;
+
+public class ActionCriteria {
+}
--- src/main/java/com/flowcontrolback/components/report/ReportRepository.java
+++ src/main/java/com/flowcontrolback/components/report/ReportRepository.java
@@ -1,10 +1,11 @@
 package com.flowcontrolback.components.report;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.time.Month;
 import java.util.Optional;
 
-public interface ReportRepository extends JpaRepository<Report, Long> {
+public interface ReportRepository extends JpaRepository<Report, Long>, JpaSpecificationExecutor<Report> {
     Optional<Report> findByMonthAndYear(Month month, Integer year);
 }
