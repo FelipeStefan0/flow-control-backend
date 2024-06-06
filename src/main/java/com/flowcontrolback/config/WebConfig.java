4f41cd91df411a762bdf986896fd2ace21e25fba
felipe.stefano <felipe.stefano@parnamirim.rn.gov.br>
2024-06-06 12:41:03 -0300

    support: modifying web config file to fix CORS and adding a variable to front url access

--- src/main/java/com/flowcontrolback/config/WebConfig.java
+++ src/main/java/com/flowcontrolback/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.flowcontrolback.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,15 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${flow.control.front.url}")
+    private String flowControlFrontUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry
-                .addMapping("/api/**")
-                .allowedOrigins("http://localhost:8100")
-                .allowedMethods("**")
-                .allowedHeaders("**")
-                .allowedHeaders("Access-Control-Origin-Header")
-                .exposedHeaders("**")
-                .maxAge(3600);
+                .addMapping("/**")
+                .allowedOrigins(flowControlFrontUrl)
+                .allowedMethods("*")
+                .allowCredentials(true);
     }
 }
