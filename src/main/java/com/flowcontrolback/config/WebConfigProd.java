b43b1959ab4c5a218ff62e8476df41d95f5d4ca5
FelipeStefan0 <felipemarquesdestefano@gmail.com>
2024-09-19 11:14:54 -0300

    Support: creating web config in separated files and setting prod profile

--- src/main/java/com/flowcontrolback/config/WebConfigProd.java
+++ src/main/java/com/flowcontrolback/config/WebConfigProd.java
@@ -1,6 +1,5 @@
 package com.flowcontrolback.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,17 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@Profile("dev")
-public class WebConfig implements WebMvcConfigurer {
-    @Value("${flow.control.front.url}")
-    private String flowControlFrontUrl;
-
+@Profile("prod")
+public class WebConfigProd implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedOrigins(flowControlFrontUrl)
+                .allowedOrigins("*")
                 .allowedMethods("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
